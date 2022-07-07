c498175e8d04edfefc8897f9fe2afd6c1d6d3335
ZimsGoated <michaeliswoodruff@gmail.com>
2022-07-07 12:25:53 -0500

    move Aptt.tsx into index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import styled from '@emotion/styled';
+import { Global, css } from '@emotion/react';
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  background-color: var(--background-color);
+`;
+
+const CSSVariables = css`
+  :root {
+    --background-color: #ffffff;
+  }
+`;
+
+const GlobalStyle = css`
+  ${CSSVariables};
+  body {
+    margin: 0;
+  }
+`;
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Global styles={GlobalStyle} />
+      <AppContainer></AppContainer>
+    </React.Fragment>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
